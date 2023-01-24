674fb1c9cb7e92436f2afec2f27ce96e5ea50abe
Selwyn Simsek <selwyn.simsek@quantinuum.com>
2023-01-24 14:17:59 +0000

    Fix README and add zotxt

--- packages.el
+++ packages.el
@@ -20,51 +20,17 @@
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-;;; Commentary:
 
-;; See the Spacemacs documentation and FAQs for instructions on how to implement
-;; a new layer:
-;;
-;;   SPC h SPC layers RET
-;;
-;;
-;; Briefly, each package to be installed or configured by this layer should be
-;; added to `zotero-packages'. Then, for each package PACKAGE:
-;;
-;; - If PACKAGE is not referenced by any other Spacemacs layer, define a
-;;   function `zotero/init-PACKAGE' to load and initialize the package.
+(defconst zotero-packages '(zotxt))
 
-;; - Otherwise, PACKAGE is already referenced by another Spacemacs layer, so
-;;   define the functions `zotero/pre-init-PACKAGE' and/or
-;;   `zotero/post-init-PACKAGE' to customize the package as it is loaded.
-
-;;; Code:
-
-(defconst zotero-packages
-  '()
-  "The list of Lisp packages required by the zotero layer.
-
-Each entry is either:
-
-1. A symbol, which is interpreted as a package to be installed, or
-
-2. A list of the form (PACKAGE KEYS...), where PACKAGE is the
-    name of the package to be installed or loaded, and KEYS are
-    any number of keyword-value-pairs.
-
-    The following keys are accepted:
-
-    - :excluded (t or nil): Prevent the package from being loaded
-      if value is non-nil
-
-    - :location: Specify a custom installation location.
-      The following values are legal:
-
-      - The symbol `elpa' (default) means PACKAGE will be
-        installed using the Emacs package manager.
-
-      - The symbol `local' directs Spacemacs to load the file at
-        `./local/PACKAGE/PACKAGE.el'
-
-      - A list beginning with the symbol `recipe' is a melpa
-        recipe.  See: https://github.com/milkypostman/melpa#recipe-format")
+(defun zotero/init-zotxt ()
+  "Inspired by <http://www.mkbehr.com/posts/a-research-workflow-with-zotero-and-org-mode/>."
+  (add-hook 'org-mode-hook (lambda () (org-zotxt-mode 1)))
+  ;; Bind something to replace the awkward C-u C-c " i
+  (define-key org-mode-map
+              (kbd "C-c \" \"") (lambda () (interactive)
+                                  (org-zotxt-insert-reference-link '(4))))
+  ;; Change citation format to be less cumbersome in files.
+  ;; You'll need to install mkbehr-short into your style manager first.
+  (eval-after-load "zotxt"
+    '(setq zotxt-default-bibliography-style "mkbehr-short")))
